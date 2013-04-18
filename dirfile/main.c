7b95102cd220789b644be833a79435995f5aef48
lublud <luido992@gmail.com>
2013-04-18 15:44:48 +0200

    Modifications for calculating page frame's number.

--- dirfile/main.c
+++ dirfile/main.c
@@ -14,40 +14,52 @@
 
 void CalculDuNombreDeCadre ()
 {
-	unsigned i;
-	double result;
+	unsigned i, MemoireManquante;
 
+	/* Mémoire vive */
 	NombreCadreMemoireVive = TailleMemoireVive / TailleCadrePages;
-	printf ("RAM creation (%d frame(s))\n", NombreCadreMemoireVive);
 
-	/*
-	if ()
+	MemoireManquante = TailleMemoireVive - (TailleCadrePages * NombreCadreMemoireVive);
+	if (MemoireManquante >  0)
 		++NombreCadreMemoireVive;
-	*/
+
+	printf ("RAM creation (%d frame(s))\n", NombreCadreMemoireVive);
 
 	CadrePageMemVive = (SProcessus *)
 					malloc (NombreCadreMemoireVive * sizeof (SProcessus));
-	CadrePageMemViveRestante = (unsigned *) malloc (NombreCadreMemoireVive * sizeof (unsigned));
+	CadrePageMemViveRestante = (unsigned *)
+					malloc (NombreCadreMemoireVive * sizeof (unsigned));
+
 
 	for (i = 0; i < NombreCadreMemoireVive; ++i)
 		CadrePageMemViveRestante [i] = TailleCadrePages;
 
+	if (MemoireManquante > 0)
+		CadrePageMemViveRestante [i - 1] = MemoireManquante;
 
+
+
+	/* Mémoire virtuelle */
 	NombreCadreMemoireVirtuelle = TailleMemoireVirtuelle / TailleCadrePages;
-	printf ("Virtual memory creation (%d frame(s))\n", NombreCadreMemoireVirtuelle);
 
-	/*
-	if ()
+	MemoireManquante = TailleMemoireVirtuelle - (TailleCadrePages * NombreCadreMemoireVirtuelle);
+	if (MemoireManquante > 0)
 		++NombreCadreMemoireVirtuelle;
-	*/
+
+	printf ("Virtual memory creation (%d frame(s))\n", NombreCadreMemoireVirtuelle);
 
 	CadrePageMemVirtuelle = (SProcessus *)
 					malloc (NombreCadreMemoireVirtuelle * sizeof (SProcessus));
-	CadrePageMemVirtuelleRestante = (unsigned *) malloc (NombreCadreMemoireVirtuelle * sizeof (unsigned));
+	CadrePageMemVirtuelleRestante = (unsigned *)
+					malloc (NombreCadreMemoireVirtuelle * sizeof (unsigned));
+
 	NbCadreMemVirtuelleLibre = NombreCadreMemoireVirtuelle;
 
 	for (i = 0; i < NombreCadreMemoireVirtuelle; ++i)
 		CadrePageMemVirtuelleRestante [i] = TailleCadrePages;
+
+	if (MemoireManquante > 0)
+		CadrePageMemVirtuelleRestante [i - 1] = MemoireManquante;
 
 } // CalculDuNombreDeCadre ()
 
